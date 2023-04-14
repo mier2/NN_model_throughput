8d21f73397ce8c23cf4732abc928efca63ae158f
Ubuntu <ubuntu@ip-10-0-151-213.us-west-2.compute.internal>
2023-04-14 19:49:12 +0000

    csv file setup

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import torch
-from ray.air import session, Checkpoint, ScalingConfig
+from ray.air import session, Checkpoint, ScalingConfig, Result
 from ray import train
 from models import PolicyNN_60, PolicyNN_48, PolicyNN_24, PolicyNN_23, PolicyNN_108, PolicyNN_211
 from ray.train.torch import TorchTrainer
+import csv
 
-
-
+#traning model function
 def train_func(config):
+    #define the training model
     model = train.torch.prepare_model(config["modelName"])
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
     
-    input = torch.randn(1000, 60)
-    labels = torch.randn(1000, 8)
+    #generate toy data
+    input = torch.randn(1000, config["dim_num"])
+    labels = torch.randn(1000, config["label_dim"])
     dataset = torch.utils.data.TensorDataset(input, labels)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=32)
     dataloader = train.torch.prepare_data_loader(dataloader)
@@ -37,11 +39,8 @@ def train_func(config):
         
     
     
-    
-
-
-
-config = {"modelName": PolicyNN_60(), "num_epoch":1}
+#run the training model
+config = {"modelName": PolicyNN_60(), "num_epoch":1, "dim_num":60, "label_dim":8 }
 trainer = TorchTrainer(
     train_loop_per_worker=train_func,
     train_loop_config=config,
@@ -50,7 +49,37 @@ trainer = TorchTrainer(
         num_workers=1,
         )
     )
-result = trainer.fit() 
-print(result.checkpoint.to_dict())
+result: Result = trainer.fit()
+
+print(result.metrics)
+
+
+
+
+
+
+
+
+
+batch_size = 32
+#ouput the throughput into a csv file
+iterations = result.metrics.get('training_iteration')
+time = result.metrics.get("time_total_s")
+model_name = "PolicyNN_60"
+num_gpu = 1
+
+#calculate the throughput
+total_samples = batch_size * iterations
+throughput = total_samples/time
+
+data = [
+    ["Model Dim#", "Number of GPUS", "Throughput"],
+    [model_name, num_gpu, throughput]
+]
+
+with open("results.csv", "w", newline="") as csvfile:
+    csv_writer= csv.writer(csvfile)
+    csv_writer.writerows(data)
+
     
     
